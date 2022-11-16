1b0d169a1f854aad8e6f52fbbf1e63c49c30c057
Shawn Kebel <fxrender@outlook.com>
2022-11-16 01:15:43 -0600

    :sparkles: Add `FeaturedInfo` section of `Home` page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 
 export default function Home() {
   return (
     <div className="home">
+      <FeaturedInfo />
       <div className="homeWidgets">
       </div>
     </div>
