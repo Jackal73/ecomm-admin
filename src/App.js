777a0cecb78fb8ff751c52522d6d8178c465390d
Shawn Kebel <fxrender@outlook.com>
2022-11-16 23:00:28 -0600

    :sparkles: Create `add NewUser` page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 
 import "./app.css";
+import NewUser from "./pages/newUser/NewUser";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
           </Route>
           <Route path="/user/:userId">
             <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
