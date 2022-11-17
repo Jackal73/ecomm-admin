17da5e907d606c4494b8c4e56a61499f479e36da
Shawn Kebel <fxrender@outlook.com>
2022-11-16 22:54:29 -0600

    ;sparkles: create user `display` and`update`

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 import "./app.css";
 
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path="/users">
             <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
           </Route>
         </Switch>
       </div>
